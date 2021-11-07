f62d6713680a36b9494f06150fe74e4840db8e0e
James Boehme <jamesrbdev@gmail.com>
2021-11-06 19:10:16 -0500

    Responsive adjustments. Better fading. Organization and comments.

--- public/script.js
+++ public/script.js
@@ -1,29 +1,61 @@
+/*
+	Hello! Interested in offering me a position at your company?
+	Drop me an email at: jamesrbdev@gmail.com 
+*/
+
+//——————————————————————————————————————————————————————————————————//
+// INFORMATION                                                      //
+//——————————————————————————————————————————————————————————————————//
+
+// Contributors: James Boehme
+
+// This script handles all of the front-end web functionality.
+
+//——————————————————————————————————————————————————————————————————//
+// VARIABLES                                                        //
+//——————————————————————————————————————————————————————————————————//
+
 let currentCat;
 let catQuestions;
 
-function loadQuestions() {
+//——————————————————————————————————————————————————————————————————//
+// FUNCTIONS                                                        //
+//——————————————————————————————————————————————————————————————————//
+
+console.log("%cHello! Let me know how I can improve by dropping me an email at: %cjamesrbdev@gmail.com", "font-size: 18px;", "font-size: 18px; color: lightblue; text-decoration: underline;")
+
+// Loads questions array from a text file.
+function loadQuestions() { // ()
 	fetch(`questions/${currentCat}.txt`).then(response => response.text()).then((data) => {
 		catQuestions = data.split("\n");
 		generate();
 	});
 }
 
-function setCategory(category) {
+// Sets the current question category.
+function setCategory(category) { // (string)
 	let catOrb = document.getElementById("cat-" + category);
 	let catP = catOrb.getElementsByTagName("p")[0];
 	let genC = document.getElementById("gen-container")
+
 	genC.hidden = undefined;
+
 	catOrb.style.padding = "1280px";
-	catOrb.style.zIndex = 100;
+	catOrb.style.zIndex  = 100;
+
 	document.getElementById("orb-container").style.pointerEvents = "none";
+	
 	catP.style.opacity = 0;
-	genC.style.opacity = 1;
+
+	setTimeout(() => genC.style.opacity = 1, 0);
+
 	currentCat = category;
 
 	loadQuestions();
 }
 
-function generate() {
+// Generates a random question from the list.
+function generate() { // ()
 	if (catQuestions.length < 1) {
 		loadQuestions();
 	} else {
@@ -33,14 +65,20 @@ function generate() {
 	}
 }
 
-function showAbout() {
+// Shows the about info.
+function showAbout() { // ()
 	let catOrb = document.getElementById("title-orb");
 	let catP = catOrb.getElementsByTagName("h1")[0];
 	let genC = document.getElementById("about-container")
+
 	genC.hidden = undefined;
+
 	catOrb.style.padding = "1280px";
-	catOrb.style.zIndex = 100;
+	catOrb.style.zIndex  = 100;
+
 	document.getElementById("orb-container").style.pointerEvents = "none";
+
 	catP.style.opacity = 0;
-	genC.style.opacity = 1;
+
+	setTimeout(() => genC.style.opacity = 1, 0);
 }
